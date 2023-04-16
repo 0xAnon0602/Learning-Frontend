8ca1186320d8eab4e6a0a1edd726fc117de09087
0xAnon <utkarshshail005@protonmail.com>
2023-04-16 16:13:48 +0530

    TODO MORE JS ADDED

--- todo-react-tailwind-app/src/components/MainBody.js
+++ todo-react-tailwind-app/src/components/MainBody.js
@@ -1,4 +1,6 @@
-import {React,useState,useEffect} from 'react'
+import  React,{useState,useEffect} from 'react'
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const MainBody = () => {
 
@@ -17,13 +19,42 @@ useEffect(() => {
   }
 }, []);
 
+const renderTodo  = () => {
+  const todoLength = todoItems.length
+  if(todoLength===0){var height=120}
+  else{height = 90*todoLength}
+
+  var allItems = []
+
+  todoItems.forEach(element => {
+    allItems.push(
+    <div className='border bg-white rounded-lg mt-2 mb-2 flex flex-row items-center'>
+    <span className='w-[730px] h-[55px] py-4 indent-2  '>{element['title']}</span>
+    <IconButton aria-label="delete" size="small">
+    <DeleteIcon fontSize="small" onClick={(x)=>{console.log(x.target.value)}}/>
+    </IconButton>    
+    </div>
+    )
+  });
+
+  if(allItems.length===0){
+    allItems.push(
+      <p className=' font-bold text-lg border text-[#585858] bg-[#DEDFE1] max-w-[200px] rounded-xl text-center py-1 px-2'>No Todos</p>
+      )
+  }
+
+  return(
+    <div className={`border w-[300px] md:w-[780px] h-[${height}px] mx-auto mt-3 bg-[#ECEDF6] rounded-xl flex flex-col items-center justify-center`}>
+    {allItems}
+    </div>
+    )
+}
+
 const submitNewData = () => {
   var newData = {'title':newTitle,'type':newType}
   todoItems.push(newData)
   localStorage.setItem('todo_items',JSON.stringify(todoItems))
-  console.log(todoItems)
   setTaskButtonValue(!taskButtonValue)
-
 }
 
   return (
@@ -39,14 +70,27 @@ const submitNewData = () => {
        <option value="Incompleted">Incompleted</option>
      </select>
 
-     <div className='border w-[300px] md:w-[780px] h-[90px] mx-auto mt-3 bg-[#ECEDF6] rounded-xl flex items-center justify-center'>
+     {/* <div className='border w-[300px] md:w-[780px] h-[180px] mx-auto mt-3 bg-[#ECEDF6] rounded-xl flex flex-col items-center justify-center'>
+     {todoItems.length===0 ? 
      <p className=' font-bold text-lg border text-[#585858] bg-[#DEDFE1] max-w-[200px] rounded-xl text-center py-1 px-2'>No Todos</p>
+      :
+      <>
+     <div className='border bg-white rounded-lg'>
+     <p className='flex w-[750px] h-[55px] py-2 indent-2'>singh</p>
      </div>
+     <div className='border bg-white rounded-lg mt-3'>
+     <p className='flex w-[750px] h-[55px] py-2 indent-2'>c</p>
+     </div>
+     </>
+     }
+     </div>  */}
+
+     <div>{renderTodo()}</div>
 
      {taskButtonValue && (
     <div className="fixed inset-0 backdrop-blur-[1.5px] flex items-center justify-center"> 
     
-    <div className=' w-[450px] mx-auto h-[300px] mt-60 bg-[#ECEDF6] rounded-xl inset-0 backdrop-blur-sm bg-opacity-25'>
+    <div className=' w-[450px] mx-auto h-[300px] mt-60 bg-[#CCCDDE] rounded-xl inset-0 backdrop-blur-sm bg-opacity-25'>
     <p className='font-bold text-xl ml-4 text-[#646681] py-2 mr-12 inline-block'>Add TODO</p>
      <button className="bg-white rounded-md p-2 text-gray-400 hover:text-white hover:bg-[#e64343] ml-60 "
      onClick={()=>{
@@ -58,10 +102,10 @@ const submitNewData = () => {
       </svg>
     </button> 
      <p className='mt-3 font-bold text-l ml-4 text-[#646681]'>TITLE</p>
-     <input type="text" className="p-3 input input-bordered input-accent  w-full max-w-xs mt-2 h-[50px] ml-4" onChange={(x)=>{setNewTitle(x.target.value)}}/>
+     <input type="text" className="p-3 input input-bordered input-accent  w-full max-w-xs mt-2 h-[50px] ml-4 required" onChange={(x)=>{setNewTitle(x.target.value)}}/>
      <p className='mt-4 font-bold text-l ml-4 text-[#646681]'>STATUS</p>
   
-     <select className='p-3 input input-bordered input-accent  w-full max-w-xs mt-2 h-[50px] ml-4 bg-white' onChange={(y)=>{setNewType(y.target.value)}}>
+     <select className='p-3 input input-bordered input-accent  w-full max-w-xs mt-2 h-[50px] ml-4 bg-white required' onChange={(y)=>{setNewType(y.target.value)}}>
        <option value="Completed">Completed</option>
        <option value="Incompleted">Incompleted</option>
      </select>
